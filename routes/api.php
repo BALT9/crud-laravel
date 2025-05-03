f96648231e3bae30d928b067f03a5e55c28b0774
willy <willybalt.2017@gmail.com>
2025-05-03 19:36:41 -0400

    se creo un controlador

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/students', function(){
     return 'Studenst List';
 }); 
+
+Route::get('/students/{id}', function(){
+    return 'obtener un solo estudiante';
+}); 
+
+Route::post('/students', function(){
+    return 'Creando estudiantes';
+});
+
+Route::put('/students/{id}', function(){
+    return 'Actualizando estudiante';
+});
+
+Route::delete('/students/{id}', function(){
+    return 'Eliminar estudiante';
+}); 
+ 
+
